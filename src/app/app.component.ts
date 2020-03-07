a0befec5b0f513e72d14613c8fa630a846bdeefb
PAC <PAC@NBPAC>
2020-03-07 16:28:14 -0300

    Add nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title = 'Etiel Engenharia';
   //construction1 = 'https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/87569842_106352087635012_2171636576977682432_o.png?_nc_cat=102&_nc_sid=6e5ad9&_nc_ohc=kA1HbRvGN9cAX_CcDI9&_nc_ht=scontent-gru2-2.xx&oh=ece2065df9d56cee148c34de978be5de&oe=5E98B117';
   construction1 = 'assets/image/logo-2.png';
-  construction2 = 'https://thumbs.gfycat.com/ForcefulCelebratedAzurevase-small.gif';
+  construction2 = 'assets/image/logo-200x84.png';
   construction3 = 'https://www.campogrande.ms.gov.br/sedesc/wp-content/uploads/sites/20/2018/11/output_zvmmKf.gif';
 }
