c295e8a111f6881018bba9186587535029745944
PAC <PAC@NBPAC>
2020-03-09 01:48:12 -0300

    Componentes para o menu bar

---
+++ src/app/Home/body/body.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContatoComponent } from './contato/contato.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+
+
+@NgModule({
+  declarations: [ContatoComponent, ServicosComponent, SobreComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BodyModule { }
---
+++ src/app/Home/body/servicos/servicos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ee-servicos',
+  templateUrl: './servicos.component.html',
+  styleUrls: ['./servicos.component.css']
+})
+export class ServicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
