6d7a1578c97bc2096245005c114146121e505acd
PAC <PAC@NBPAC>
2020-03-18 02:09:51 -0300

    Add carousel de servicos

--- src/app/Home/body/servicos/servicos.component.ts
+++ src/app/Home/body/servicos/servicos.component.ts
@@ -10,27 +10,32 @@ export class ServicosComponent implements OnInit {
     {
       img: './assets/image/regularizacao_de_Imoveis.jpg',
       titulo: 'Regularização de Imóveis',
-      descricao:'Regularização de Imóveis'
+      descricao:'Regularização de Imóveis',
+      active: 'active'
     },
     {
       img: './assets/image/vistorias_de _obras.jpg',
       titulo: 'Vistorias de Obras',
-      descricao:'Vistorias de Obras'
+      descricao:'Vistorias de Obras',
+      active: ''      
     },
     {
       img: './assets/image/calculo_do_valor_de_renovacao_de_locacao.jpg',
       titulo: 'Cálculo do valor de renovação de locação',
-      descricao:'Cálculo do valor de renovação de locação'
+      descricao:'Cálculo do valor de renovação de locação',
+      active: ''
     },
     {
       img: './assets/image/laudos_de_avaliacoes_de_imoveis.jpg',
       titulo: 'Laudos de Avaliações de Imóveis',
-      descricao:'Laudos de Avaliações de Imóveis'
+      descricao:'Laudos de Avaliações de Imóveis',
+      active: ''
     },
     {
       img: './assets/image/pericia_engenharia.jpg',
       titulo: 'Perícias de Engenharia',
-      descricao:'Perícias de Engenharia'
+      descricao:'Perícias de Engenharia',
+      active: ''
     }
   ];
 
