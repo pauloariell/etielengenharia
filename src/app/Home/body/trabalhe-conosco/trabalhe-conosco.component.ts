751f75f16d63bb14703239332756b270d77f0e0f
PAC <PAC@NBPAC>
2020-04-05 14:43:50 -0300

    Nova Page Trabalhe conosco

---
+++ src/app/Home/body/trabalhe-conosco/trabalhe-conosco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ee-trabalhe-conosco',
+  templateUrl: './trabalhe-conosco.component.html',
+  styleUrls: ['./trabalhe-conosco.component.css']
+})
+export class TrabalheConoscoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
