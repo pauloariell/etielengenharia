e3acbd9c179c5b262fb381e628eb7c37b6564655
PAC <PAC@NBPAC>
2020-05-14 13:08:48 -0300

    Pasta realocada dentro do projeto

--- src/app/Home/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ee-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css']
-})
-export class FooterComponent { }
--- src/app/Home/home.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ee-home',
-    templateUrl: './home.component.html'
-})
-export class HomeComponent { }
