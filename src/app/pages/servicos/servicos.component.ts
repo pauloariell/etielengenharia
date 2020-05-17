3553e09a11fa4ae4dd5141f1c7a5751978e7aa07
PAC <PAC@NBPAC>
2020-05-17 04:45:27 -0300

    Diminuindo tamanho dos cards

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -16,7 +16,37 @@ export class ServicosComponent implements OnInit {
       img: 'assets/img/no-image.jpg',
       titulo: 'Avaliações de Imóveis - Urbanos e Rurais',
       descricao:'Garantias, Patrimoniais, Desapropriação, Revisional de Aluguel, Renovatória de contrato de locação, etc'
-    }
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
   ];
 
   constructor() { }
