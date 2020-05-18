66fa56f69a65c646306170f1ff77655e3e7d9945
PAC <PAC@NBPAC>
2020-05-18 01:48:38 -0300

    Correções no estilo

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -13,11 +13,11 @@ export class ServicosComponent implements OnInit {
       titulo: 'Perícias de Engenharia',
       descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
     },
-    // {
-    //   img: 'assets/img/no-image.jpg',
-    //   titulo: 'Avaliações de Imóveis',
-    //   descricao:'Garantias, Patrimoniais, Desapropriação, Revisional de Aluguel, Renovatória de contrato de locação, etc'
-    // },
+    {
+      img: 'assets/img/no-image.jpg',
+      titulo: 'Avaliações de Imóveis',
+      descricao:'Garantias, Patrimoniais, Desapropriação, Revisional de Aluguel, Renovatória de contrato de locação, etc'
+    },
     // {
     //   img: 'assets/img/servico-1.jpg',
     //   titulo: 'Perícias de Engenharia',
