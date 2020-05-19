57fcc68a896bd2092cab4b121dc365aa4806f23e
PAC <PAC@NBPAC>
2020-05-19 01:11:43 -0300

    Add Footer Style

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// import { HomeComponent } from './home/home.component';
-// import { ServicosComponent } from './servicos/servicos.component';
-// import { HistoriaComponent } from './historia/historia.component';
-// import { NoticiasComponent } from './noticias/noticias.component';
-// import { TrabalheConoscoComponent } from './trabalhe-conosco/trabalhe-conosco.component';
-// import { ContatoComponent } from './contato/contato.component';
 import { EmBreveComponent } from './em-breve/em-breve.component';
 import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
