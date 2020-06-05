d6d8cf2febe6f1513580eae5c6a302fdd7d5d250
PAC <PAC@NBPAC>
2020-06-05 12:47:12 -0300

    Add capa nova, com tamanho de imagem mais leve

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   images = [
-    'assets/img/capa-01.png',
-    'assets/img/capa-02.png',
-    'assets/img/capa-03.png',
-    'assets/img/capa-04.png',
+    'assets/img/jpg/capa-dev.jpg',
   ];
   constructor() { }
 
