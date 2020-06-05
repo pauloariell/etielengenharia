ce122479409b580a7247a2c5774d8b360b86ebc6
PAC <PAC@NBPAC>
2020-06-05 15:07:12 -0300

    Aprimorando envio de email

--- src/app/core/send-mail/send-mail.service.ts
+++ src/app/core/send-mail/send-mail.service.ts
@@ -21,16 +21,17 @@ sendMessage(data) {
   URL_API = this.url_prd;
   console.log(`[URL_API]${URL_API}`)
 
-  const body = {
-    emailto: data.email,
-    subject: "[SITE] Contato Etiel",
-    text: `Você tem um novo contato\n
-            Nome: ${data.name}\n
-            E-mail:${data.email}\n
-            Telefone:${data.phone}\n
-            Assunto:${data.description}`
-  }
-  console.log(JSON.stringify(body));
-  return this._http.post(`${this.url_dev}/sendmail`, body);
+  // const body = {
+  //   emailto: data.email,
+  //   subject: "[SITE] Contato Etiel",
+  //   name:data.name,
+  //   text: `Você tem um novo contato\n
+  //           Nome: ${data.name}\n
+  //           E-mail:${data.email}\n
+  //           Telefone:${data.phone}\n
+  //           Assunto:${data.description}`
+  // }
+  console.log(JSON.stringify(data));
+  return this._http.post(`${this.url_dev}/sendmail`, data);
 }
 }
