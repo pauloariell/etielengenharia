b18a2b4e2dd67b79cc4bb5d7b4610069fa883b36
PAC <PAC@NBPAC>
2020-06-09 12:58:04 -0300

    [Refactory] Limpando codigo

--- src/app/core/send-mail/send-mail.service.ts
+++ src/app/core/send-mail/send-mail.service.ts
@@ -11,27 +11,17 @@ export class SendMailService {
   constructor(private _http: HttpClient) { }
 
 
-sendMessage(data) {
-  let URL_API = '';
-  if (isDevMode) {
-    URL_API = this.url_dev;
-  } else {
+  sendMessage(data) {
+    let URL_API = '';
+    if (isDevMode) {
+      URL_API = this.url_dev;
+    } else {
+      URL_API = this.url_prd;
+    }
     URL_API = this.url_prd;
-  }
-  URL_API = this.url_prd;
-  console.log(`[URL_API]${URL_API}`)
+    console.log(`[URL_API]${URL_API}`)
 
-  // const body = {
-  //   emailto: data.email,
-  //   subject: "[SITE] Contato Etiel",
-  //   name:data.name,
-  //   text: `Você tem um novo contato\n
-  //           Nome: ${data.name}\n
-  //           E-mail:${data.email}\n
-  //           Telefone:${data.phone}\n
-  //           Assunto:${data.description}`
-  // }
-  console.log(JSON.stringify(data));
-  return this._http.post(`${URL_API}/sendmail`, data);
-}
+    console.log(JSON.stringify(data));
+    return this._http.post(`${URL_API}/sendmail`, data);
+  }
 }
