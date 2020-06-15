cb7889f0040ecc917307109b8cbfaeda66a5e57f
PAC <PAC@NBPAC>
2020-06-14 21:13:23 -0300

    .

--- src/app/pages/trabalhe-conosco/enviarcv/enviarcv.component.ts
+++ src/app/pages/trabalhe-conosco/enviarcv/enviarcv.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,24 +7,68 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./enviarcv.component.scss']
 })
 export class EnviarcvComponent implements OnInit {
+  public showOverlay = false;
   enviacvForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
+    private cd: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
     this.enviacvForm = this.formBuilder.group({
-      name: ['',[Validators.required]],
+      firstname: ['',[Validators.required]],
+      lastname: ['',[Validators.required]],
       email: ['',[Validators.required, Validators.email]],
       phone: ['',[Validators.required]],
+      uf: ['',[Validators.required]],
+      city: ['',[Validators.required]],
       description: ['',[Validators.required]],
+      file: [null, Validators.required],
     });
   }
 
 
   onSubmit(){
-    alert("Olar!")
+    this.turnOnOverlay()
+    if (this.enviacvForm.invalid) {
+      console.log(`[DATA error]: ${JSON.stringify(this.enviacvForm.value)}`);
+      this.turnOffOverlay()
+      return;
+    }
+    console.log(`[DATA SUCESS]: ${JSON.stringify(this.enviacvForm.value)}`);
+    this.turnOffOverlay()
   }
+
+  onFileChange(event) {
+    console.log('[Reader] - Iniciando');
+    let reader = new FileReader();
+
+    if(event.target.files && event.target.files.length) {
+      console.log(`[Reader] - ${JSON.stringify(event.target.files)}`);
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      console.log(`[Reader] - ${reader.result}`);
+
+      reader.onload = () => {
+        this.enviacvForm.patchValue({
+          file: reader.result
+        });
+
+        // // need to run CD since file load runs outside of zone
+        // this.cd.markForCheck();
+      };
+    }
+  }
+
+  turnOnOverlay() {
+    this.showOverlay = true;
+    alert('teste')
+  };
+
+  turnOffOverlay() {
+    this.showOverlay = false;
+  };
 
 }
