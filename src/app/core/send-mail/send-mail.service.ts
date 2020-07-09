3d6c747c2cfb603191ad0e056478e038381e1d56
PAC <PAC@NBPAC>
2020-07-09 00:25:22 -0300

    Implementando conversão de arquivo PDF para Base64

--- src/app/core/send-mail/send-mail.service.ts
+++ src/app/core/send-mail/send-mail.service.ts
@@ -1,17 +1,30 @@
 import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface contactUs {
+  name: string;
+  email: string;
+  phone: string;
+  description: string;
+  fileAttachment?: fileAttachment
+}
+
+interface fileAttachment{
+  filename: string,
+  content: string
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SendMailService {
   url_dev = 'http://localhost:3001/api';
-  url_prd = 'https://etielengenharia-api.herokuapp.com/api'
+  url_prd = 'https://etielengenharia-api.herokuapp.com/api';
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
+  sendMessage({ name, email, phone, description }: contactUs) {
+    const data = { name, email, phone, description };
 
-  sendMessage(data) {
     let URL_API = '';
     if (isDevMode) {
       URL_API = this.url_dev;
@@ -19,9 +32,25 @@ export class SendMailService {
       URL_API = this.url_prd;
     }
     URL_API = this.url_prd;
-    console.log(`[URL_API]${URL_API}`)
+    console.log(`[URL_API]${URL_API}`);
 
     console.log(JSON.stringify(data));
     return this._http.post(`${URL_API}/sendmail`, data);
+  }
+
+  sendMailWorkwithUs({ name, email, phone, description, fileAttachment }: contactUs) {
+    const data = { name, email, phone, description, fileAttachment };
+
+    let URL_API = '';
+    if (isDevMode) {
+      URL_API = this.url_dev;
+    } else {
+      URL_API = this.url_prd;
+    }
+    URL_API = this.url_prd;
+    console.log(`[URL_API]${URL_API}`);
+
+    console.log(JSON.stringify(data));
+    return this._http.post(`${URL_API}/sendMailWorkwithUs`, data);
   }
 }
--- src/app/pages/trabalhe-conosco/enviarcv/enviarcv.component.ts
+++ src/app/pages/trabalhe-conosco/enviarcv/enviarcv.component.ts
@@ -1,74 +1,98 @@
-import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ee-enviarcv',
   templateUrl: './enviarcv.component.html',
-  styleUrls: ['./enviarcv.component.scss']
+  styleUrls: ['./enviarcv.component.scss'],
 })
 export class EnviarcvComponent implements OnInit {
   public showOverlay = false;
   enviacvForm: FormGroup;
+  fileToUpload: string;
+  typefile = 'application/pdf';
 
   constructor(
     private formBuilder: FormBuilder,
-    private cd: ChangeDetectorRef,
+    private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.enviacvForm = this.formBuilder.group({
-      firstname: ['',[Validators.required]],
-      lastname: ['',[Validators.required]],
-      email: ['',[Validators.required, Validators.email]],
-      phone: ['',[Validators.required]],
-      uf: ['',[Validators.required]],
-      city: ['',[Validators.required]],
-      description: ['',[Validators.required]],
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+      uf: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      description: ['', [Validators.required]],
       file: [null, Validators.required],
     });
   }
 
-
-  onSubmit(){
-    this.turnOnOverlay()
-    if (this.enviacvForm.invalid) {
-      console.log(`[DATA error]: ${JSON.stringify(this.enviacvForm.value)}`);
-      this.turnOffOverlay()
-      return;
-    }
+  onSubmit() {
+    console.log(`[Submit] - start`);
+    this.turnOnOverlay();
+    // if (this.enviacvForm.invalid) {
+    //   console.log(`[DATA error]: ${JSON.stringify(this.enviacvForm.value)}`);
+    //   this.turnOffOverlay()
+    //   return;
+    // }
     console.log(`[DATA SUCESS]: ${JSON.stringify(this.enviacvForm.value)}`);
-    this.turnOffOverlay()
+
+    var { file } = this.enviacvForm.value;
+    console.log(`[DATA SUCESS]: ${JSON.stringify(file)}`);
+    this.turnOffOverlay();
+
+    // const formData: FormData = new FormData();
+    // formData.append('fileKey', this.fileToUpload, this.fileToUpload.name);
+    // console.log(`[DATA] - formData: ${JSON.stringify(formData)}`)
   }
 
-  onFileChange(event) {
-    console.log('[Reader] - Iniciando');
-    let reader = new FileReader();
-
-    if(event.target.files && event.target.files.length) {
-      console.log(`[Reader] - ${JSON.stringify(event.target.files)}`);
-      const [file] = event.target.files;
-      reader.readAsDataURL(file);
-
-      console.log(`[Reader] - ${reader.result}`);
-
-      reader.onload = () => {
-        this.enviacvForm.patchValue({
-          file: reader.result
-        });
-
-        // // need to run CD since file load runs outside of zone
-        // this.cd.markForCheck();
-      };
+  onFileChange(files: FileList) {
+    this.turnOnOverlay();
+    if (files && files.length) {
+      var file = files.item(0);
+      if (file.type == this.typefile) {
+        this.convertFiletoBase64(file);
+        this.turnOffOverlay();
+        return;
+      } else {
+        alert('Num é um PDF cacete!');
+        this.turnOffOverlay();
+        return;
+      }
+      // this.fileToUpload = files.item(0);
     }
+    this.turnOffOverlay();
+  }
+
+  loadFileBase64(file: string) {
+    console.log(`[FileConvert]->${file}`);
+    return false;
+  }
+
+  convertFiletoBase64(file) {
+    var reader = new FileReader();
+
+    reader.onload = (function (file) {
+      return function (e) {
+        var binaryData = e.target.result;
+        //Converting Binary Data to base 64
+        var base64String = window.btoa(binaryData);
+        //showing file converted to base64
+        this.fileToUpload = base64String;
+        console.log(`[FileConverting] - b64: ${this.fileToUpload}`);
+      };
+    })(file);
+    reader.readAsBinaryString(file);
   }
 
   turnOnOverlay() {
     this.showOverlay = true;
-    alert('teste')
-  };
+  }
 
   turnOffOverlay() {
     this.showOverlay = false;
-  };
-
+  }
 }
